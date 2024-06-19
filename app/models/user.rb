7584711597f13e9ffda835f32a6f8e3c693ea895
HenryHung90 <hungwork510031@gmail.com>
2024-06-19 15:07:06 +0800

    add validates for users and tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@
 # User Model
 class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
+
+  validates :username, presence: true, uniqueness: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :role_id, inclusion: { in: %w[admin user] }
 end
