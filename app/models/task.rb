dad2623370b9ac43caf23a7c856ee34d609c7f77
HenryHung90 <hungwork510031@gmail.com>
2024-06-20 16:26:08 +0800

    add model enum

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,11 +5,11 @@ class Task < ApplicationRecord
   belongs_to :user
   has_many :task_tags, dependent: :destroy
   has_many :tags, through: :task_tags
+  enum priority: { high: 'high', medium: 'medium', low: 'low' }
 
   accepts_nested_attributes_for :tags, allow_destroy: true
   # scope :complete_before, ->(date) { where('end_time < ?', date) }
   scope :status_done, -> { where(status: 'done') }
-
   validates :title, presence: true, length: { maximum: 100 }
   validates :content, presence: true, length: { maximum: 500 }
   validates :priority, presence: true, inclusion: { in: %w[high medium low] }
