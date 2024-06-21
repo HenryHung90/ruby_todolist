21862bc2705a122360fce695cc82e4ec233596a2
HenryHung90 <hungwork510031@gmail.com>
2024-06-21 12:28:59 +0800

    rubocop problem fix

--- db/migrate/20240621041128_add_priority_int_to_tasks.rb
+++ db/migrate/20240621041128_add_priority_int_to_tasks.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Migration
 class AddPriorityIntToTasks < ActiveRecord::Migration[7.1]
   def change
     add_column :tasks, :priority_int, :integer, default: 2, null: false
--- db/migrate/20240621041212_remove_old_priority_and_rename_new_priority.rb
+++ db/migrate/20240621041212_remove_old_priority_and_rename_new_priority.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# RemoveOldPriorityAndRenameNewPriority
 class RemoveOldPriorityAndRenameNewPriority < ActiveRecord::Migration[7.1]
   def change
     remove_column :tasks, :priority, :string
