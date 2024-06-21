5e8fea2b3ec9f4413c57c6001655abc2608b7328
HenryHung90 <hungwork510031@gmail.com>
2024-06-21 18:26:40 +0800

    sort by scope

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,13 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
-    @tasks = filter_tasks(@user.tasks).page(params[:page]).per(10)
+    @user = User.find_by(username: params[:id])
+    @tasks = @user.tasks
+                  .includes(:tags)
+                  .sort_by_date_and_priority(params[:sort])
+                  .filter_by_status(params[:status])
+                  .filter_by_title(params[:title])
+                  .page(params[:page]).per(10)
   end
 
   def new; end
@@ -25,36 +30,4 @@ class UsersController < ApplicationController
   def update; end
 
   def destory; end
-
-  private
-
-  # 篩選 Task
-  def filter_tasks(tasks)
-    tasks = sort_tasks(tasks)
-    tasks = filter_by_status(tasks)
-    filter_by_title(tasks)
-  end
-
-  # 透過 join start end date 排序
-  def sort_tasks(tasks)
-    sort_by = params[:sort_by].presence_in(SORTABLE_COLUMNS) || 'created_at'
-    return tasks.order(sort_by => :asc) if sort_by != 'priority'
-
-    tasks.order(Arel.sql("CASE priority WHEN 'high' THEN 1 WHEN 'medium' THEN 2 WHEN 'low' THEN 3 END") => :asc)
-  end
-
-  # 透過 status 篩選
-  def filter_by_status(tasks)
-    status = params[:status].presence || 'all'
-    return tasks if status == 'all'
-
-    tasks.where(status:)
-  end
-
-  # ILIKE PostgreSQL 特有操作, ILIKE 不區分大小寫
-  # ? 佔位字符, 防止 SQL Injection -> 後一個為傳遞給 ? 的實際值
-  def filter_by_title(tasks)
-    title = params[:title].presence || ''
-    tasks.where('title ILIKE ?', "%#{title}%")
-  end
 end
