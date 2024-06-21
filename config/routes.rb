8aea333d374106d47e11ed241d0da5bc185d18fa
HenryHung90 <hungwork510031@gmail.com>
2024-06-21 18:23:43 +0800

    add login 相關

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
+  get 'login' => 'session#new'
+  post 'login' => 'session#create'
+  delete 'logout' => 'session#destroy'
+
   resources :users do
     resources :tasks, except: [:index]
   end
@@ -14,6 +18,7 @@ Rails.application.routes.draw do
     resources :manage_tasks
     resources :manage_users
   end
+
   # Defines the root path route ("/")
-  root 'page#index'
+  root 'session#new'
 end
