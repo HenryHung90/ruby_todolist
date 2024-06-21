ec7f4dad87e5d9899213d1581f8918ee71b62628
HenryHung90 <hungwork510031@gmail.com>
2024-06-21 18:24:50 +0800

    add middle ware function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
+# middle ware function
 class ApplicationController < ActionController::Base
+  before_action :require_login
+
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  private
+
+  def require_login
+    return if current_user
+
+    redirect_to login_path, alert: I18n.t('notices.must_login')
+  end
 end
