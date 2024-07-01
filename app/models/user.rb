331f1805f8e377404d97f37c003c899ba081e873
HenryHung90 <hungwork510031@gmail.com>
2024-07-01 14:37:45 +0800

    add scope sort_by_date_or_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :tasks, dependent: :destroy
+
+  scope :sort_by_date_or_id, lambda { |sort_by|
+    sort_by = sort_by.presence_in(%w[id created_at updated_at]) || 'id'
+    order(sort_by => :asc)
+  }
+
   validates :username, presence: true, length: { maximum: 50 }
   validates :name, presence: true, length: { maximum: 30 }
   validates :password, presence: true, length: { minimum: 6 }
