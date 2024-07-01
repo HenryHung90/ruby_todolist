438601581a46e921bcc4ff850a2c5b4ddbf04490
HenryHung90 <hungwork510031@gmail.com>
2024-07-01 15:54:08 +0800

    filter 改用 scope 製作 before_action

--- app/controllers/admin/manage_tasks_controller.rb
+++ app/controllers/admin/manage_tasks_controller.rb
@@ -3,17 +3,22 @@
 module Admin
   # ManageTaskController
   class ManageTasksController < ApplicationController
+    before_action :authorize_admin
+    before_action :set_task, except: %i[create new]
     layout 'backend'
     # sort by date
     SORTABLE_COLUMNS = %w[created_at start_time end_time priority].freeze
 
     def index
-      @tasks = filter_tasks(Task).page(params[:page]).per(10)
+      @tasks = @tasks.sort_by_date_and_priority(params[:sort])
+                     .filter_by_status(params[:status])
+                     .filter_by_title(params[:title])
+                     .page(params[:page]).per(10)
       @users = User.all
     end
 
     def show
-      @task = Task.find(params[:id])
+      @task
     end
 
     def new
@@ -21,7 +26,7 @@ module Admin
     end
 
     def edit
-      @task = Task.find(params[:id])
+      @task = @task.find(params[:id])
     end
 
     def create
@@ -34,7 +39,7 @@ module Admin
     end
 
     def update
-      @task = Task.find(params[:id])
+      @task = @tasks.find(params[:id])
       if @task.update(task_params)
         redirect_to admin_manage_task_path(@task), notice: I18n.t('notices.task_updated')
       else
@@ -43,41 +48,21 @@ module Admin
     end
 
     def destroy
-      @task = Task.find(params[:id])
+      @task = @tasks.find(params[:id])
       @task.destroy
       redirect_to admin_manage_tasks_path, notice: I18n.t('notices.task_destroyed')
     end
 
     private
 
-    # 篩選 Task
-    def filter_tasks(tasks)
-      tasks = sort_tasks(tasks)
-      tasks = filter_by_status(tasks)
-      filter_by_title(tasks)
+    def set_task
+      @tasks = Task.includes(:tags)
     end
 
-    # 透過 join start end date 排序
-    def sort_tasks(tasks)
-      sort_by = params[:sort_by].presence_in(SORTABLE_COLUMNS) || 'created_at'
-      return tasks.order(sort_by => :asc) if sort_by != 'priority'
+    def authorize_admin
+      return if current_user&.role_id == 'admin'
 
-      tasks.order(Arel.sql("CASE priority WHEN 'high' THEN 1 WHEN 'medium' THEN 2 WHEN 'low' THEN 3 END") => :asc)
-    end
-
-    # 透過 status 篩選
-    def filter_by_status(tasks)
-      status = params[:status].presence || 'all'
-      return tasks if status == 'all'
-
-      tasks.where(status:)
-    end
-
-    # ILIKE PostgreSQL 特有操作, ILIKE 不區分大小寫
-    # ? 佔位字符, 防止 SQL Injection -> 後一個為傳遞給 ? 的實際值
-    def filter_by_title(tasks)
-      title = params[:title].presence || ''
-      tasks.where('title ILIKE ?', "%#{title}%")
+      redirect_to root_path, alert: 'You are not authorized to access this page.'
     end
 
     def task_params
