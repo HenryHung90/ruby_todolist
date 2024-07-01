9b14878d4a28db4bf496f1f7ed42980d5ffee438
HenryHung90 <hungwork510031@gmail.com>
2024-07-01 14:35:37 +0800

    增加 user sort function

--- app/controllers/admin/manage_users_controller.rb
+++ app/controllers/admin/manage_users_controller.rb
@@ -5,11 +5,11 @@ module Admin
   class ManageUsersController < ApplicationController
     def index
       @users = User.all
+                   .sort_by_date_or_id(params[:sort_by])
+                   .page(params[:page]).per(10)
     end
 
-    def show
-      @user = User.find(params[:id])
-    end
+    def show; end
 
     def new
       @user = User.new
@@ -24,16 +24,16 @@ module Admin
       if @user.save
         redirect_to admin_manage_user_path(@user), notice: I18n.t('notices.user_created')
       else
-        render :new
+        render :new, status: :unprocessable_entity
       end
     end
 
     def update
       @user = User.find(params[:id])
       if @user.update(user_params)
-        redirect_to admin_manage_user_path(@user), notice: I18n.t('notices.task_updated')
+        redirect_to admin_manage_users_path, notice: I18n.t('notices.user_updated')
       else
-        render :edit
+        render :edit, status: :unprocessable_entity
       end
     end
 
