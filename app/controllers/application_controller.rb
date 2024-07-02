a2773bb327dc388cc4b82498233329a6790d05e0
HenryHung90 <hungwork510031@gmail.com>
2024-07-02 10:06:03 +0800

    配置 404 & 500 page controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,21 @@
 # middle ware function
 class ApplicationController < ActionController::Base
   before_action :require_login
-
   helper_method :current_user
+  rescue_from StandardError, with: :render_error
+  rescue_from ActiveRecord::RecordNotFound, with: :render_no_found
+
+  def render_error(exception)
+    logger.error(exception.message)
+    logger.error(exception.backtrace.join("\n"))
+    render file: Rails.public_path.join('500.html').to_s, layout: false, status: :internal_server_error
+  end
+
+  def render_no_found
+    render file: Rails.public_path.join('404.html').to_s, layout: false, status: :not_found
+  end
+
+  private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
