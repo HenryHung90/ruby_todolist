cf28dec43c8237d564e5927152e48fae6c2d3234
HenryHung90 <hungwork510031@gmail.com>
2024-07-02 11:08:47 +0800

    seed and fix database

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,30 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+User.destroy_all
+Task.destroy_all
+
+10.times do
+  user = User.create!(
+    username: Faker::Internet.unique.username,
+    name: Faker::Name.name,
+    password: 'password',
+    role_id: %w[admin user].sample
+  )
+  rand(20..30).times do
+    Task.create!(
+      title: Faker::Lorem.sentence(word_count: 3),
+      content: Faker::Lorem.paragraph,
+      priority: %w[high medium low].sample,
+      status: %w[pending progress done].sample,
+      start_time: Faker::Time.backward(days: 14, period: :morning),
+      end_time: Faker::Time.forward(days: 14, period: :evening),
+      user:
+    )
+  end
+end
+
+puts "Created #{User.count} users with #{Task.count} tasks."
+Ｆ
